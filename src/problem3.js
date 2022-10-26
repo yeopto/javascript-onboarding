95fc6233eccfb49f6ca0270ffda540756cf277e4
yeopto <gunyeop530@gmail.com>
2022-10-26 22:08:53 +0900

    Feat: threeSixNineCount 함수 구현
    숫자에 3 or 6 or 9가 몇개인지 구하는 함수를 구현하다.

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,15 @@
+function threeSixNineCount(num) {
+  let count = 0;
+  for (let i = 0; i < String(num).length; i++) {
+    if (
+      String(num)[i] === '3' ||
+      String(num)[i] === '6' ||
+      String(num)[i] === '9'
+    ) count++;  
+  }
+  return count;
+}
+
 function problem3(number) {
   var answer;
   return answer;
