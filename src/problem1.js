4cfdafa654d51c6f11d9163cadb099e6a7988e57
yeopto <gunyeop530@gmail.com>
2022-10-26 17:59:06 +0900

    Feat: 문제 1 완료
    1. 1부터 400인지 확인하는 함수
    2. Input 길이가 2인지 확인하는 함수
    3. 다음 페이지가 맞는지 확인하는 함수
    4. 각 요소 별 자리 수 덧셈, 곱셈 후 큰 수 구하는 함수

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,54 @@
+function isInPage(pobiPages, crongPages) {
+  if (
+    pobiPages.every((page) => page >= 1 && page <= 400) && 
+    crongPages.every((page) => page >= 1 && page <= 400)
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function isLengthTwo(pobiPages, crongPages) {
+  if (
+    pobiPages.length === 2 && 
+    crongPages.length === 2
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function checkNextPage(pobiPages, crongPages) {
+  if (
+    pobiPages[1] - pobiPages[0] === 1 &&
+    crongPages[1] - crongPages[0] === 1
+  ) {
+    return true;
+  }
+  return false;
+}
+
+function getBiggerNum(page) {
+  const add = String(page).split('').reduce((acc, cur) => +acc + +cur, 0);
+  const multiply = String(page).split('').reduce((acc, cur) => +acc * +cur, 1);
+  const biggerNum = Math.max(add, multiply);
+
+  return biggerNum;
+}
+
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if (
+    !isInPage(pobi, crong) ||
+    !isLengthTwo(pobi, crong) ||
+    !checkNextPage(pobi, crong)
+  ) return -1;
+
+  const pobiScore = Math.max(...pobi.map((pobiPage) => getBiggerNum(pobiPage)))
+  const crongScore = Math.max(...crong.map((crongPage) => getBiggerNum(crongPage)));
+
+  if (pobiScore === crongScore) return 0;
+  else if (pobiScore > crongScore) return 1;
+  else return 2;
 }
 
 module.exports = problem1;
