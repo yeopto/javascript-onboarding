7d9a5725cdc1c82a443faf5bf8c46cfb4cda1f24
yeopto <gunyeop530@gmail.com>
2022-10-27 04:29:54 +0900

    Feat: 문제 4, 대소문자 판별 후 문자 변경하는 로직 함수 구현
    checkLowCaseOrUpperCase 함수 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,13 +1,17 @@
+function checkLowCaseOrUpperCase(ascii) {
+  if (ascii >= 65 && ascii <= 90) 
+    return String.fromCharCode(65 + 90 - ascii);
+  else if (ascii >= 97 && ascii <= 122) 
+    return String.fromCharCode(97 + 122 - ascii);
+  else
+    return String.fromCharCode(ascii);
+}
+
 function problem4(word) {
-  return word.split("").map((el) => {
-    let ascii = el.charCodeAt();
-    if (ascii >= 65 && ascii <= 90) 
-      return String.fromCharCode(65 + 90 - ascii);
-    else if (ascii >= 97 && ascii <= 122) 
-      return String.fromCharCode(97 + 122 - ascii);
-    else
-      return String.fromCharCode(ascii);
-  }).join('');
+  return word
+    .split("")
+    .map(splitWord => checkLowCaseOrUpperCase(splitWord.charCodeAt()))
+    .join('');
 }
 
 module.exports = problem4;
