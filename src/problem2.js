e3c3ab5643d61f899c0805b3f95cd6d5a0945973
yeopto <gunyeop530@gmail.com>
2022-10-26 20:37:03 +0900

    Feat: 문제 2  완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,17 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const cryptogramArr = [...cryptogram];
+  const stack = [];
+
+  for (let i = 0; i < cryptogramArr.length; i++) {
+    if (cryptogramArr[i] !== cryptogramArr[i + 1]) {
+      stack.push(cryptogramArr[i]);
+    } else {
+      i++;
+    }
+  }
+  const decoding = stack.join('');
+
+  return decoding === cryptogram ? decoding : problem2(decoding);
 }
 
 module.exports = problem2;
