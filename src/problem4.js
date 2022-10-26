ec4c4a123499e65c2a53e028b76ed4a895ef7b5c
yeopto <gunyeop530@gmail.com>
2022-10-27 03:32:52 +0900

    Feat: 문제 4 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,13 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  return word.split("").map((el) => {
+    let ascii = el.charCodeAt();
+    if (ascii >= 65 && ascii <= 90) 
+      return String.fromCharCode(60 + 95 - ascii);
+    else if (ascii >= 97 && ascii <= 122) 
+      return String.fromCharCode(97 + 122 - ascii)
+    else
+      return String.fromCharCode(ascii)
+  }).join('');
 }
 
 module.exports = problem4;
