82983425bb5f42279538637f983d83f09094075d
yeopto <gunyeop530@gmail.com>
2022-10-27 21:18:25 +0900

    Feat: 문제 6, forms 내 모든 크루 이름을 두 글자씩 나누어 중복 수 확인하는 객체 생성 로직 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,17 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const result = new Set();
+  const checkOverlapObj = {};
+
+  forms.map((form) => {
+    const [_, crewName] = form;
+    for (let i = 0; i < crewName.length - 1; i++) {
+      const tempWord = crewName.substring(i, i + 2);
+      if (tempWord in checkOverlapObj) checkOverlapObj[tempWord]++;
+      else checkOverlapObj[tempWord] = 1;
+    }
+  });
+  
+  return result;
 }
 
 module.exports = problem6;
