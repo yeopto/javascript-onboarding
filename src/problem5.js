00e56e71804b26af00b98fd76eabdf4ce34d9222
yeopto <gunyeop530@gmail.com>
2022-10-27 16:56:58 +0900

    Feat: 문제 5 완료

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,11 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const paperMoney = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  return paperMoney.map((el => {
+    const divideValue = Math.floor(money / el);
+    money %= el;
+    return divideValue;
+  }));
 }
 
 module.exports = problem5;
