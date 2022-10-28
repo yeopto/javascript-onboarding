f7d370d0920dccd3a7dfb93a4e9a6da2c20b26a7
yeopto <gunyeop530@gmail.com>
2022-10-28 21:17:45 +0900

    Feat: 문제 7 완료
    최대 5명 리턴하는 로직, 추천 점수가 같은 경우 이름순 정렬 로직 구현 완료

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,47 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const recommendScoreObj = {};
+
+  const userFriends = friends
+    .filter((el) => el.includes(user))
+    .flat()
+    .filter((el) => el !== user);
+
+  const recommandedFriends = friends.filter((el) => {
+    const [A, B] = el;
+    if (
+      (userFriends.includes(A) || userFriends.includes(B)) &&
+      !el.includes(user)
+    ) return el;
+  });
+
+  for (const recommandedFriend of recommandedFriends) {
+    const [A, B] = recommandedFriend;
+    if (A in recommendScoreObj) recommandedFriend[A] += 10;
+    else if (B in recommendScoreObj) recommandedFriend[B] += 10;
+    else {
+      if (userFriends.includes(A)) recommendScoreObj[B] = 0;
+      else recommendScoreObj[A] = 0;
+    }
+  }
+
+  for (const visitor of visitors) {
+    if (visitor in recommendScoreObj) recommendScoreObj[visitor] += 1;
+    else recommendScoreObj[visitor] = 1;
+  }
+
+  const result = [];
+  for (const name in recommendScoreObj) {
+    if (!userFriends.includes(name)) result.push([name, recommendScoreObj[name]]);
+  }
+
+  return result
+    .sort((a, b) => {
+      if (b[1] > a[1]) return 1;
+      if (a[1] < b[1]) return -1;
+      if (a[1] === b[1]) return a[0] < b[0] ? -1 : 1;
+    })
+    .map((el) => el[0])
+    .slice(0, 5);
 }
 
 module.exports = problem7;
