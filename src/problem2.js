9be364506bb40b31062b9ef7319e730261ae2b36
yeopto <gunyeop530@gmail.com>
2022-10-30 18:38:47 +0900

    Refactor: 문제 2, 명령형을 선언형으로 변경
    for문 대신 map 메서드를 사용

--- src/problem2.js
+++ src/problem2.js
@@ -1,21 +1,21 @@
 function problem2(cryptogram) {
   const cryptogramArr = [...cryptogram];
-  const stack = [];
   let checkOverword = "";
 
-  for (let i = 0; i < cryptogramArr.length; i++) {
-    if (checkOverword === cryptogramArr[i]) continue;
-
-    if (cryptogramArr[i] === cryptogramArr[i + 1])
-      checkOverword = cryptogramArr[i];
-    else {
-      stack.push(cryptogramArr[i]);
+  const decoding = cryptogramArr
+    .map((word, index) => {
+      if (checkOverword === word) return null;
+      if (cryptogramArr[index] === cryptogramArr[index + 1]) {
+        checkOverword = cryptogramArr[index];
+        return null;
+      }
       checkOverword = "";
-    }
-  }
-  const decoding = stack.join("");
+      return word;
+    })
+    .join("");
 
-  return decoding === cryptogram ? decoding : problem2(decoding);
+  if (decoding === cryptogram) return decoding 
+  return problem2(decoding);
 }
 
 module.exports = problem2;
